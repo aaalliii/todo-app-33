5234852a0d828c984750c6093fafba9969d705a4
alimov m <jj24ers@gmail.com>
2023-06-05 17:42:11 +0600

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -48,21 +49,22 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Todo App</h1>
+    <div className="app">
+      <div class="big-text">todos</div>
       <form onSubmit={addTask}>
         <input
           type="text"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
         />
-        <button onClick={addTask}>Add Task</button>
       </form>
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
             {task}
-            <button onClick={() => deleteTask(index)}>Delete</button>
+            <button class="li-button" onClick={() => deleteTask(index)}>
+              x
+            </button>
           </li>
         ))}
       </ul>
